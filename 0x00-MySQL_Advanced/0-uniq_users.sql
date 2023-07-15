3e6ecd0c77b492f711c420d4a3aab7860bd918b6
Kanu Anthony <kanuchibueze@gmail.com>
2023-07-15 16:40:39 +0000

    task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,12 @@
+-- Creates a table 'users'
+-- with attributes:
+-- id: integer, never null, auto increment and primary key
+-- email: string(255 characters), nevel null
+-- name string(255 character)
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255) 
+	
+);
