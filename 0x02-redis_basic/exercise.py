416b3e784f1915c65a54729b5c239c6b0da4ec64
kanu Anthony <kanuchibueze@gmail.com>
2023-07-21 20:10:18 +0000

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,26 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float, None]:
+        """retrievs data from Redis
+        """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn is not None:
+            data = fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """paramize cache.get to string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """parametrize cache.get int
+        """
+        return self.get(key, fn=int)
