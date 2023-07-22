c1814c7e50a8f368af642fba0434a93748d19405
kanu Anthony <kanuchibueze@gmail.com>
2023-07-22 02:48:40 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,8 +5,7 @@
 import redis
 import uuid
 from typing import Union, Callable
-from functools import wraps
-
+from functiontools import wraps
 
 class Cache:
     def __init__(self):
@@ -21,8 +20,30 @@ class Cache:
 
         return key
 
+    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float, None]:
+        """retrievs data from Redis
+        """
+        data = self._redis.get(key)
 
-def count_calls(method: callable) -> callable:
+        if data is None:
+            return None
+
+        if fn is not None:
+            data = fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """paramize cache.get to string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """parametrize cache.get int
+        """
+        return self.get(key, fn=int)
+
+def count_calls(method: Callable) -> Callable:
     """ Incrementing Value """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
