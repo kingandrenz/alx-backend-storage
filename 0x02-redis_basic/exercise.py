fa885a208d37a8ec02abb697a8ffa0b1ec7b6463
kanu Anthony <kanuchibueze@gmail.com>
2023-07-22 03:04:39 +0000

    task2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,58 +2,29 @@
 """ Writing strings to Redis
 """
 
-import redis
 import uuid
+import redis
 from typing import Union, Callable
-from functiontools import wraps
+import functools
 
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
-        self._redis.flushdb()
 
-    @wraps
+    @functools.wraps
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
         """ store the input in Redis"""
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
-
         return key
-
-    def get(self, key: str, fn: callable = None) -> Union[str, bytes,
-                                                          int, float, None]:
-        """retrievs data from Redis
-        """
-        data = self._redis.get(key)
-
-        if data is None:
-            return None
-
-        if fn is not None:
-            data = fn(data)
-
-        return data
-
-    def get_str(self, key: str) -> Union[str, None]:
-        """paramize cache.get to string
-        """
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        """parametrize cache.get int
-        """
-        return self.get(key, fn=int)
 
 
 def count_calls(method: Callable) -> Callable:
-    """ Incrementing Value
-    """
-    @wraps(method)
+    """ Incrementing Value """
+    @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
         key = method.__qualname__
         self._redis.incr(key)
-
         return method(self, *args, **kwargs)
-
     return wrapper
