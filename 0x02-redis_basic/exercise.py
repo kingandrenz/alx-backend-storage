5b45f5c87cd21289ad24637f95f745e6b576ecc8
kanu Anthony <kanuchibueze@gmail.com>
2023-07-22 02:34:39 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,8 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+from functtools import wraps
 
 
 class Cache:
@@ -12,6 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @wraps
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         """ store the input in Redis"""
@@ -19,25 +21,14 @@ class Cache:
 
         return key
 
-    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float, None]:
-        """retrievs data from Redis
-        """
-        data = self._redis.get(key)
 
-        if data is None:
-            return None
+def count_calls(method: Callable) -> Callable:
+    """ Incrementing Value """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
 
-        if fn is not None:
-            data = fn(data)
+        return method(self, *args, **kwargs)
 
-        return data
-
-    def get_str(self, key: str) -> Union[str, None]:
-        """paramize cache.get to string
-        """
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        """parametrize cache.get int
-        """
-        return self.get(key, fn=int)
+    return wrapper
